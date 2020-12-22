a48676b2c9a07f5e78dba85eb1894828a86d54b8
Prajit Lal <prajit.lal4@gmail.com>
2020-12-23 00:09:08 +0800

    progress

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -43,6 +43,20 @@ function ContactPage() {
 
           <label
             className="block mb-2 text-xs font-bold uppercase"
+            htmlFor="email"
+          >
+            Email
+          </label>
+
+          <input
+            className="w-full mb-6 rounded-md border-gray-800"
+            id="email"
+            placeholder="bill.murray@email.com"
+            type="text"
+          />
+
+          <label
+            className="block mb-2 text-xs font-bold uppercase"
             htmlFor="message"
           >
             Message
@@ -55,7 +69,7 @@ function ContactPage() {
             rows="8"
           />
 
-          <button className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
+          <button type="submit" className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
             Submit
           </button>
         </form>
